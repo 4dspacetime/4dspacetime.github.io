defa7fada7af0fdf111f18322b781119555dd6f5
Wang Yilan 王祎岚 <13974808202@163.com>
2026-01-01 15:31:26 +0800

    proto

--- tpl/tpl.js
+++ tpl/tpl.js
@@ -10,11 +10,4 @@ onload = () =>
 {
     hljs.highlightAll()
     new ClipboardJS('.copy')
-    new Gitalk({
-        clientID: 'Ov23liVMXJybGQUAvoSV',
-        clientSecret: '72bc7c14ae51864eb9eb50d5b9d1ef22f1d6d350',
-        repo: 'gitalk',
-        owner: '4dspacetime',
-        admin: ['4dspacetime']
-    }).render('gitalk-container')
 }
