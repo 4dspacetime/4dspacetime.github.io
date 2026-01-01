39f1ea462c8922443b965c819455abee44a3986e
Wang Yilan 王祎岚 <13974808202@163.com>
2026-01-01 15:11:13 +0800

    proto

--- tpl/tpl.js
+++ tpl/tpl.js
@@ -13,8 +13,9 @@ onload = () =>
     new Gitalk({
         clientID: 'Ov23liVMXJybGQUAvoSV',
         clientSecret: '72bc7c14ae51864eb9eb50d5b9d1ef22f1d6d350',
-        repo: 'https://github.com/4dspacetime/gitalk',
+        repo: 'gitalk',
         owner: '4dspacetime',
         admin: ['4dspacetime']
     }).render('gitalk-container')
+
 }
